d17f271a012b26163969f04104782f8470bb544a
R. Anthony Porter <rah@sheenonline.biz>
2017-03-26 01:04:07 -0400

    Secured bookRoutes and profile with custom middleware.

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var mongodb = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
+var ObjectId = require('mongodb').ObjectID;
 
 var bookRouter = express.Router();
 
 var router = function (nav) {
+
+    bookRouter.use(function(req,res,next) {
+        if (!req.user) {
+            res.redirect('/');
+        }
+        next();
+    });
 
     bookRouter.route('/')
         .get(function (req, res) {
@@ -27,7 +34,7 @@ var router = function (nav) {
 
     bookRouter.route('/:id')
         .get(function (req, res) {
-            var id = new objectId(req.params.id);
+            var id = new ObjectId(req.params.id);
             var url = 'mongodb://localhost:27017/libraryApp';
 
             mongodb.connect(url, function (err, db) {
